72e5ab427ad095c90875a8ba9fdc2527c98c45cb
JhonatanLira <jhonatanlira@gmail.com>
2023-06-20 04:15:25 -0300

    Aulas de 1 a 5

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+
+  name : String = "Jhonatan";
+  idade :number = 35;
+  cidade = "Cabo Frio";
+  hobbies = ["Mangá","música","academia"];
+
+constructor() {}
+
+ngOnInit(): void{}
 
 }
